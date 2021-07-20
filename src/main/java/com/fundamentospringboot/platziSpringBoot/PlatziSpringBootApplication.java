af5c4675c484ad56a7064487a45220f42c944314
unknown <alexyair028@gmail.com>
2021-07-20 11:27:49 -0500

    first commit

---
+++ src/main/java/com/fundamentospringboot/platziSpringBoot/PlatziSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentospringboot.platziSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziSpringBootApplication.class, args);
+	}
+
+}
